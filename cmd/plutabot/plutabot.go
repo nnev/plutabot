92468bc8054ccaa4a96ae393468a4160b3fdf154
Michael Stapelberg <michael@stapelberg.de>
2019-11-16 21:43:46 +0100

    cope with nick in use, recover nick

--- cmd/plutabot/plutabot.go
+++ cmd/plutabot/plutabot.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"log"
-	"strings"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/robustirc/bridge/robustsession"
+	"gopkg.in/sorcix/irc.v2"
 )
 
 func logic() error {
@@ -12,14 +16,51 @@ func logic() error {
 	if err != nil {
 		return err
 	}
-	session.PostMessage("NICK pluta")
-	session.PostMessage("USER pluta pluta pluta pluta")
-	session.PostMessage("JOIN #chaos-hd")
-	for msg := range session.Messages {
-		log.Printf("<- %s", msg)
-		if strings.HasPrefix(msg, "PING ") {
-			session.PostMessage("PONG " + strings.TrimPrefix(msg, "PING "))
-			log.Printf("-> pong")
+
+	{
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, os.Interrupt)
+		signal.Notify(c, syscall.SIGTERM)
+		go func() {
+			s := <-c
+			log.Printf("Got signal: %v", s)
+			session.Delete(fmt.Sprintf("signal %v", s))
+		}()
+	}
+
+	postMessage := func(msg *irc.Message) error {
+		log.Printf("-> %s", msg)
+		return session.PostMessage(msg.String())
+	}
+	cmd := func(command string, params ...string) error {
+		return postMessage(&irc.Message{
+			Command: command,
+			Params:  params,
+		})
+	}
+
+	const desiredNick = "pluta"
+
+	nick := desiredNick
+	cmd(irc.NICK, nick)
+	cmd(irc.USER, "pluta", "pluta", "pluta", "pluta")
+	for rawmsg := range session.Messages {
+		message := irc.ParseMessage(rawmsg)
+		log.Printf("<- %s", message)
+		switch message.Command {
+		case irc.RPL_WELCOME: // logged in
+			cmd(irc.JOIN, "#chaos-hd")
+
+		case irc.ERR_NICKNAMEINUSE: // nickname already in use
+			nick = nick + "_"
+			cmd(irc.NICK, nick)
+
+		case irc.PING:
+			cmd(irc.PONG, message.Params...)
+
+		case irc.NICK:
+		case irc.QUIT:
+			cmd(irc.NICK, desiredNick) // best effort
 		}
 	}
 	return nil
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/nnev/plutabot
 
 go 1.13
 
-require github.com/robustirc/bridge v1.7.3
+require (
+	github.com/robustirc/bridge v1.7.3
+	github.com/sorcix/irc v1.1.4-0.20170501124343-8becc86e7db2
+	gopkg.in/sorcix/irc.v2 v2.0.0-20190306112350-8d7a73540b90
+)
